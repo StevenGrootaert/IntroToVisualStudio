2e39507e2c7e9f42b8247c3459c240705d945747
StevenGrootaert <steve.grootaert@gmail.com>
2020-08-25 16:23:13 -0400

    Add project files.

---
+++ GreeterConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreeterConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args) // this is the entry point for the project running
+        {
+            string usersName = GetName();
+            Console.WriteLine("Hello, " + usersName);
+            Console.ReadLine();
+        }
+        static string GetName()
+        {
+            Console.WriteLine("Hey! What's your name?");
+            string name = Console.ReadLine();
+            return name;
+        }
+    }
+}
